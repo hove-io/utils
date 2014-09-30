032e84f082fef795ed215001deebece42e07463e
antoine-de <antoine.desbordes@gmail.com>
2014-09-30 16:54:56 +0200

    Add serialization for std::vector<std::unique_ptr<T>>

--- serialization_unique_ptr_container.h
+++ serialization_unique_ptr_container.h
@@ -2,25 +2,25 @@
   
 This file is part of Navitia,
     the software to build cool stuff with public transport.
- 
+
 Hope you'll enjoy and contribute to this project,
     powered by Canal TP (www.canaltp.fr).
 Help us simplify mobility and open public transport:
     a non ending quest to the responsive locomotion way of traveling!
-  
+
 LICENCE: This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-   
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU Affero General Public License for more details.
-   
+
 You should have received a copy of the GNU Affero General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
-  
+
 Stay tuned using
 twitter @navitia 
 IRC #navitia on freenode
@@ -31,36 +31,46 @@ www.navitia.io
 
 #include <boost/serialization/serialization.hpp>
 #include <boost/serialization/version.hpp>
-#include <boost/serialization/split_free.hpp>
+#include <boost/serialization/split_member.hpp>
 #include <memory>
 
+#include <boost/serialization/wrapper.hpp>
+
+/**
+ * Serialization specialization for std::vector<std::unique_ptr> because unique_ptr are not copiable
+ */
 namespace boost {
 namespace serialization {
 
-template<class Archive, class T>
-inline void save(Archive& ar, const std::unique_ptr<T>& t, const unsigned int){
-    // only the raw pointer has to be saved
-    const T* const tx = t.get();
-    ar << tx;
+template<class Archive, class Allocator, class T>
+inline void save(Archive& ar, const std::vector<std::unique_ptr<T>, Allocator>& t,
+        const unsigned int /* file_version */){
+    // record number of elements
+    collection_size_type count (t.size());
+    ar << BOOST_SERIALIZATION_NVP(count);
+
+    for(const auto& elt: t)
+        ar << boost::serialization::make_nvp("item", elt);
 }
 
-template<class Archive, class T>
-inline void load(Archive& ar, std::unique_ptr<T>& t, const unsigned int){
-    T* pTarget;
-    ar >> pTarget;
-
-    #if BOOST_WORKAROUND(BOOST_DINKUMWARE_STDLIB, == 1)
-        t.release();
-        t = std::unique_ptr< T >(pTarget);
-    #else
-        t.reset(pTarget);
-    #endif
+template<class Archive, class Allocator, class T>
+inline void load(Archive & ar, std::vector<std::unique_ptr<T>, Allocator>& t,
+        const unsigned int /* file_version */){
+    // retrieve number of elements
+    collection_size_type count;
+    ar >> BOOST_SERIALIZATION_NVP(count);
+    t.clear();
+    while(count-- > 0){
+        std::unique_ptr<T> i;
+        ar >> boost::serialization::make_nvp("item", i);
+        t.push_back(std::move(i));// move object
+    }
 }
 
-template<class Archive, class T>
-inline void serialize(Archive& ar, std::unique_ptr<T>& t,
+template<class Archive, class Allocator, class T>
+inline void serialize(Archive & ar, std::vector<std::unique_ptr<T>, Allocator>& t,
         const unsigned int file_version){
     boost::serialization::split_free(ar, t, file_version);
 }
-} // namespace serialization
-} // namespace boost
+}
+}
