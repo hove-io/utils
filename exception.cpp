1b34fb236548b84cd70861d79f3db81067e1f770
xlqian <patrick.qian@canaltp.fr>
2016-07-22 15:06:34 +0200

    add coord_conversion_exception

--- exception.cpp
+++ exception.cpp
@@ -4,5 +4,6 @@ namespace navitia {
 
 exception::~exception() noexcept {}
 recoverable_exception::~recoverable_exception() noexcept {}
+coord_conversion_exception::~coord_conversion_exception() noexcept{}
 
 } // namespace navitia
