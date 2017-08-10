5334cc34b83a6d24dca9a05dd4d1db47faf94aaf
Alexandre Jacquin <alexandre.jacquin@canaltp.fr>
2017-08-10 10:08:09 +0200

    better handling of errors

--- coord_parser.h
+++ coord_parser.h
@@ -5,9 +5,21 @@
 
 class wrong_coordinate: public std::runtime_error{using runtime_error::runtime_error;};
 
-// the new way to represent a coord is : "lon;lat"
-const std::string match_double = "[-+]?[0-9]*\\.?[0-9]*[eE]?[-+]?[0-9]*";
-const auto coord_regex = boost::regex("^(" + match_double + ");(" + match_double + ")$");
+namespace {
+    // the new way to represent a coord is : "lon;lat"
+    const std::string match_double = "[-+]?[0-9]*\\.?[0-9]*[eE]?[-+]?[0-9]*";
+    const auto coord_regex = boost::regex("^(" + match_double + ");(" + match_double + ")$");
+
+    std::pair<double, double> make_coord(const std::string& lon, const std::string& lat){
+        try{
+            double dlon = boost::lexical_cast<double>(lon);
+            double dlat = boost::lexical_cast<double>(lat);
+            return {dlon, dlat};
+        }catch(const boost::bad_lexical_cast&){
+            throw wrong_coordinate("conversion failed");
+        }
+    }
+}
 
 /**
  *
@@ -19,17 +31,13 @@ std::pair<double, double> parse_coordinate(const std::string& uri){
         // done for retrocompatibility
         size_t pos2 = uri.find(":", 6);
         if (pos2 != std::string::npos) {
-            double lon = boost::lexical_cast<double>(uri.substr(6, pos2 - 6));
-            double lat = boost::lexical_cast<double>(uri.substr(pos2+1));
-            return std::make_pair(lon, lat);
+            return make_coord(uri.substr(6, pos2 - 6), uri.substr(pos2+1));
         }
     }else{
         boost::smatch matches;
         bool res = boost::regex_match(uri, matches, coord_regex);
         if (res && matches.size() == 3) {
-            double lon = boost::lexical_cast<double>(matches[1]);
-            double lat = boost::lexical_cast<double>(matches[2]);
-            return std::make_pair(lon, lat);
+            return make_coord(matches[1], matches[2]);
         }
     }
     throw wrong_coordinate{"coord format invalid"};
