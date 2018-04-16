1259bd7080041c408be3ba166aaf2871d7fbe3ea
Nonifier <olivier.georget@kisio.org>
2018-04-16 11:16:02 +0200

    Add MapFind class to facilitate search in a map without dealing with iterators

---
+++ map_find.h
@@ -0,0 +1,149 @@
+/* Copyright © 2001-2018, Canal TP and/or its affiliates. All rights reserved.
+
+This file is part of Navitia,
+    the software to build cool stuff with public transport.
+
+Hope you'll enjoy and contribute to this project,
+    powered by Canal TP (www.canaltp.fr).
+Help us simplify mobility and open public transport:
+    a non ending quest to the responsive locomotion way of traveling!
+
+LICENCE: This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+Stay tuned using
+twitter @navitia
+IRC #navitia on freenode
+https://groups.google.com/d/forum/navitia
+www.navitia.io
+*/
+
+#pragma once
+
+#include <boost/optional.hpp>
+
+namespace navitia {
+namespace utils {
+
+/**
+ * @brief Find key within a map, in a functional way
+ *
+ * The find operation is performed when the object is constructed with a key,
+ * or when find() is called.
+ * Result is accessible via : if_found() or if_not_found()
+ */
+template<class Map>
+class MapFind
+{
+public:
+    typedef typename Map::key_type KeyType;
+    typedef typename Map::mapped_type ValueType;
+
+    /**
+     * @brief Create a MapFind object on a map, no find is performed until find() is called.
+     *
+     * @param map : the map to perform the search on
+     */
+    MapFind(Map & map): map(map) {}
+
+
+    /**
+     * @brief Create a MapFind object and find the key in the associated map
+     *
+     * @param map : the map to perform the search on
+     * @param key : the key to be searched in the map
+     */
+    MapFind(Map & map, const KeyType & key): map(map) {
+        find(key);
+    }
+
+    /**
+     * @brief Callback fn with the value associated to the searched key
+     * if the find was successful
+     *
+     * @param fn : a callback function that takes a mapped type as parameter
+     * @returns A MapFind object to query the restult or perfom another find
+     */
+    template<class Func>
+    MapFind& if_found(Func fn) {
+        if( res ) {
+            fn( *res );
+        }
+        return *this;
+    }
+
+    /**
+     * @brief Callback fn if nothing was found in the map
+     *
+     * @param fn : a callback function with no parameter
+     * @returns A MapFind object to query the restult or perfom another find
+     */
+    template<class Func>
+    MapFind& if_not_found(Func fn) {
+        if( !res ) {
+            fn();
+        }
+        return *this;
+    }
+
+    /**
+     * @brief Search for key within the map used in the constructor
+     *
+     * @param key : the key to be searched in the map
+     * @returns A MapFind object to query the restult or perfom another find
+     */
+    template<class Key>
+    MapFind& find(const Key & key) {
+        auto it = map.find(key);
+        if(it != map.end()) {
+            res = boost::make_optional( it->second );
+        } else {
+            res = boost::none;
+        }
+        return *this;
+    }
+
+private:
+    Map & map;
+    boost::optional<ValueType> res;
+};
+
+/**
+ * @brief Create a MapFind object with the associated map
+ *
+ * This function facilitates the contruction of a MapFind
+ * as it allowes type deduction.
+ * @param map : the map to perform the search on
+ * @returns A MapFind object to query the restult or perfom another find
+ */
+template<class Map>
+MapFind<Map> make_map_find(Map& map) {
+    return MapFind<Map>(map);
+}
+
+/**
+ * @brief Create a MapFind object with the associated map
+ *
+ * This function facilitates the contruction of a MapFind
+ * as it allowes type deduction.
+ * @param map : the map to perform the search on
+ * @param key : the key to search in the map
+ * @returns A MapFind object to query the restult or perfom another find
+ */
+template<class Map, class Key>
+MapFind<Map> make_map_find(Map& c, const Key& key) {
+    return MapFind<Map>(c, key);
+}
+
+} // namespace utils
+} // namespace navitia
---
+++ tests/map_find_test.cpp
@@ -0,0 +1,103 @@
+/* Copyright © 2001-2018, Canal TP and/or its affiliates. All rights reserved.
+
+This file is part of Navitia,
+    the software to build cool stuff with public transport.
+
+Hope you'll enjoy and contribute to this project,
+    powered by Canal TP (www.canaltp.fr).
+Help us simplify mobility and open public transport:
+    a non ending quest to the responsive locomotion way of traveling!
+
+LICENCE: This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+Stay tuned using
+twitter @navitia
+IRC #navitia on freenode
+https://groups.google.com/d/forum/navitia
+www.navitia.io
+*/
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE map_find_test
+
+#include "utils/map_find.h"
+
+#include <boost/test/unit_test.hpp>
+#include <map>
+#include <unordered_map>
+#include <string>
+
+using namespace navitia::utils;
+using std::string;
+
+namespace {
+    struct MapFind_fixture
+    {
+        std::map<uint32_t, string> map;
+        std::unordered_map<uint32_t, string> umap;
+
+        MapFind_fixture() {
+            map[1] = "one";
+            map[2] = "two";
+            map[3] = "three";
+
+            umap[1] = "one";
+            umap[2] = "two";
+            umap[3] = "three";
+        }
+    };
+} // namespace
+
+BOOST_FIXTURE_TEST_CASE(MapFind_should_find_key_in_map, MapFind_fixture) {
+
+    string found_2;
+    string not_found_2;
+
+    make_map_find(map, 2)
+        .if_found([&](const string& str){ found_2  = str; })
+        .if_not_found([&](){ not_found_2 = "Not Found"; });
+
+    BOOST_CHECK_EQUAL(found_2, "two");
+    BOOST_CHECK_EQUAL(not_found_2, "");
+}
+
+BOOST_FIXTURE_TEST_CASE(MapFind_should_not_find_key_not_in_map, MapFind_fixture) {
+
+    string found_42;
+    string not_found_42;
+
+    make_map_find(map, 42)
+        .if_found([&](const string&){ found_42  = "forty two"; })
+        .if_not_found([&](){ not_found_42 = "Not Found"; });
+
+    BOOST_CHECK_EQUAL(found_42, "");
+    BOOST_CHECK_EQUAL(not_found_42, "Not Found");
+}
+
+BOOST_FIXTURE_TEST_CASE(MapFinder_should_also_work_on_unordered_map, MapFind_fixture) {
+
+    string two;
+    string forty_two;
+
+    make_map_find(umap)
+        .find(2)
+            .if_found([&](const string& s){ two = s; })
+            .if_not_found([&]()           { two = "Not found"; })
+        .find(42)
+            .if_found([&](const string& s){ forty_two = s; })
+            .if_not_found([&]()           { forty_two = "Not found"; });
+
+    BOOST_CHECK_EQUAL(two, "two");
+    BOOST_CHECK_EQUAL(forty_two, "Not found");
+}
